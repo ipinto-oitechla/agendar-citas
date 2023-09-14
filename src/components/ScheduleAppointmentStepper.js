1c5db3b10a33bfd1b91d4024dd831f65a745f9c8
ipinto-oitechla <ipinto@oitechla.com>
2023-09-14 11:18:43 -0600

    Clean: delete imports

--- src/components/ScheduleAppointmentStepper.js
+++ src/components/ScheduleAppointmentStepper.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import StepOneForm from "./StepOneForm";
 import StepTwoForm from "./StepTwoForm";
