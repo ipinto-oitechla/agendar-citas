9291e071e297f51cf51a6b280a4212ad7a55a8db
ipinto-oitechla <ipinto@oitechla.com>
2023-09-18 08:47:45 -0600

    Fix: responsive logo

--- src/pages/ScheduleAppointmentPage.js
+++ src/pages/ScheduleAppointmentPage.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Box } from '@mui/material'
-import ScheduleAppointmentStepper from '../components/ScheduleAppointmentStepper'
+import React from "react";
+import { Box } from "@mui/material";
+import ScheduleAppointmentStepper from "../components/ScheduleAppointmentStepper";
 import logo from "../logo.png";
 
 const ScheduleAppointmentPage = () => {
   return (
     <Box textAlign="center" mt={2}>
-      <img src={logo} alt="logo" style={{ height: "20vh" }}/>
-        <ScheduleAppointmentStepper />
+      <img src={logo} alt="logo" style={{ width: "45vw" }} />
+      <ScheduleAppointmentStepper />
     </Box>
-  )
-}
+  );
+};
 
-export default ScheduleAppointmentPage
+export default ScheduleAppointmentPage;
