12770ef9c44db7a635cb7eb7f35f290acccf7b5a
ipinto-oitechla <ipinto@oitechla.com>
2023-09-21 11:28:00 -0600

    Add: authenticate user to get token

--- src/pages/ScheduleAppointmentPage.js
+++ src/pages/ScheduleAppointmentPage.js
@@ -2,19 +2,26 @@ import React, { useEffect } from "react";
 import { Box } from "@mui/material";
 import ScheduleAppointmentStepper from "../components/ScheduleAppointmentStepper";
 import axios from "axios";
+import { useAuth } from "../contexts/AppointmentProvider";
 
 const ScheduleAppointmentPage = () => {
+  const { storeInfo } = useAuth();
   useEffect(() => {
     try {
-      let data = { 
-        email: process.env.REACT_APP_EMAIL,
-        password: process.env.REACT_APP_PASSWORD 
+      let data = {
+        username: process.env.REACT_APP_EMAIL,
+        password: process.env.REACT_APP_PASSWORD,
       };
 
-      axios.post(`${process.env.REACT_APP_API_URL}api-token-auth/`, data)
-      .then((res) => {
-        console.log("RES ", res.data);
-      }).catch(error => console.error(error));
+      axios
+        .post(`${process.env.REACT_APP_API_URL}api-token-auth/`, data)
+        .then((res) => {
+          if (res.status === 200) {
+            const token = res.data.token;
+            storeInfo({token});
+          }
+        })
+        .catch((error) => console.error(error));
     } catch (error) {
       throw console.error(error);
     }
